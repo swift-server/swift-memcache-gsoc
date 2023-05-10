28ade0ba19b82e222af2ddae79f9692374c7cb36
Franz Busch <f.busch@apple.com>
2023-05-10 15:36:22 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,49 @@
+// swift-tools-version: 5.7
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-memcache-gsoc open source project
+//
+// Copyright (c) 2023 Apple Inc. and the swift-memcache-gsoc project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of swift-memcache-gsoc project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-memcache-gsoc",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .watchOS(.v6),
+        .tvOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "SwiftMemcache",
+            targets: ["SwiftMemcache"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.43.1"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "SwiftMemcache",
+            dependencies: [
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "Logging", package: "swift-log"),
+            ]
+        ),
+        .testTarget(
+            name: "SwiftMemcacheTests",
+            dependencies: ["SwiftMemcache"]
+        ),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,5 @@
+# 🚧WIP🚧: SwiftMemcache
+
+SwiftMemcache is a Swift Package in development that provides a convenient way to communicate with [Memcached](https://github.com/memcached/memcached) servers.
+
+## Getting Started
