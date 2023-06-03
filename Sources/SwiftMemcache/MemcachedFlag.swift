d1a872cb76d913d4327f04386e081aac18458442
dkz2 <132388278+dkz2@users.noreply.github.com>
2023-06-03 06:29:35 -0500

    init MemcachedFlag Fixes #12 (#13)

---
+++ Sources/SwiftMemcache/MemcachedFlag.swift
@@ -0,0 +1,25 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-memcache-gsoc open source project
+//
+// Copyright (c) 2023 Apple Inc. and the swift-memcache-gsoc project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of swift-memcache-gsoc project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+enum MemcachedFlag {
+    /// v: return item value in <data block>
+    case v
+
+    var bytes: UInt8 {
+        switch self {
+        case .v:
+            return 0x76
+        }
+    }
+}
---
+++ Tests/SwiftMemcacheTests/UnitTest/MemcachedFlagTests.swift
@@ -0,0 +1,23 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-memcache-gsoc open source project
+//
+// Copyright (c) 2023 Apple Inc. and the swift-memcache-gsoc project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of swift-memcache-gsoc project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+@testable import SwiftMemcache
+import XCTest
+
+final class MemcachedFlagTests: XCTestCase {
+    func testVFlagBytes() {
+        let flag = MemcachedFlag.v
+        XCTAssertEqual(flag.bytes, 0x76)
+    }
+}
