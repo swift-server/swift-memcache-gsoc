0ac2c992a82794366b7b0c9faedd37a26e7ba1ef
dkz2 <dkzibr2@gmail.com>
2023-07-31 11:42:49 -0500

    no longer using these

--- Sources/SwiftMemcache/Extensions/UInt8+Characters.swift
+++ Sources/SwiftMemcache/Extensions/UInt8+Characters.swift
@@ -20,9 +20,7 @@ extension UInt8 {
     static var s: UInt8 = .init(ascii: "s")
     static var g: UInt8 = .init(ascii: "g")
     static var v: UInt8 = .init(ascii: "v")
-    static var t: UInt8 = .init(ascii: "t")
     static var T: UInt8 = .init(ascii: "T")
-    static var hyphen: UInt8 = .init(ascii: "-")
     static var zero: UInt8 = .init(ascii: "0")
     static var nine: UInt8 = .init(ascii: "9")
 }
