759644c6c7a615e857a46054bc7b47e09d5b1d8f
dkz2 <dkzibr2@gmail.com>
2023-10-10 22:56:01 -0500

    update benchmark script

--- dev/update-benchmark-thresholds.sh
+++ dev/update-benchmark-thresholds.sh
@@ -25,6 +25,19 @@
 ## SPDX-License-Identifier: Apache-2.0
 ##
 ##===----------------------------------------------------------------------===##
+##//===----------------------------------------------------------------------===//
+##//
+##// This source file is part of the swift-memcache-gsoc open source project
+##//
+##// Copyright (c) 2023 Apple Inc. and the swift-memcache-gsoc project authors
+##// Licensed under Apache License v2.0
+##//
+##// See LICENSE.txt for license information
+##// See CONTRIBUTORS.txt for the list of swift-memcache-gsoc project authors
+##//
+##// SPDX-License-Identifier: Apache-2.0
+##//
+##//===----------------------------------------------------------------------===//
 
 set -eu
 set -o pipefail
@@ -32,7 +45,7 @@ set -o pipefail
 here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 target_repo=${2-"$here/.."}
 
-for f in 57 58 59 510 -nightly; do
+for f in 57 58 59 -nightly; do
     echo "swift$f"
 
     docker_file=$(if [[ "$f" == "-nightly" ]]; then f=main; fi && ls "$target_repo/docker/docker-compose."*"$f"*".yaml")
